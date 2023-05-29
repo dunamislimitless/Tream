c11393964715be55396b709d3f374e2df7e9ced1
Dunamis <ogundojusamuel@gmail.com>
2023-05-29 22:56:07 +0100

    paying dues

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:arc_mobile/presentation/invoice_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    title: 'Invoice Pages',
+    home: InvoicePage(),
+  ));
+}
--- lib/presentation/invoice_page.dart
+++ lib/presentation/invoice_page.dart
@@ -7,9 +7,10 @@ class InvoicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: const Color(0XFFE5E5E5),
       body: SafeArea(
         child: Padding(
-          padding: const EdgeInsets.all(10.0),
+          padding: const EdgeInsets.all(19.0),
           child: Column(
             children: [
               Row(
@@ -46,19 +47,49 @@ class InvoicePage extends StatelessWidget {
                         fontWeight: FontWeight.w400,
                         color: Color(0XFF7B7B7B),
                       )),
-                  IconButton(onPressed: () {}, icon: Icon(Icons.settings)),
-                  IconButton(onPressed: () {}, icon: Icon(Icons.notifications)),
+                  IconButton(
+                      onPressed: () {}, icon: const Icon(Icons.settings)),
+                  IconButton(
+                      onPressed: () {}, icon: const Icon(Icons.notifications)),
                 ],
               ),
               const SizedBox(height: 10.0),
-              const Text(
-                'Invoice',
-                style: TextStyle(
-                  fontSize: 24.0,
-                  fontWeight: FontWeight.w600,
-                  color: Color(0XFF18181C),
+              Row(children: [
+                const Text(
+                  'Invoice',
+                  style: TextStyle(
+                    fontSize: 24.0,
+                    fontWeight: FontWeight.w600,
+                    color: Color(0XFF18181C),
+                  ),
                 ),
-              ), DropdownButton( , onChanged: onChanged)
+                const Spacer(),
+                Container(
+                    height: 30.0,
+                    width: 120.0,
+                    decoration: BoxDecoration(
+                        color: const Color(0XFF0B0830),
+                        borderRadius: BorderRadius.circular(16)),
+                    child: const Center(
+                      child: Text(
+                        'Request Payment',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 13.0,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                    ))
+              ]),
+              const SizedBox(height: 10.0),
+              Container(
+                width: 437,
+                height: 120,
+                decoration: BoxDecoration(
+                    color: Color(0XFF3A49F9),
+                    borderRadius: BorderRadius.circular(14)),
+              )
             ],
           ),
         ),
